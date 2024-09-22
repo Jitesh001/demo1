bdb683448ea3ae99a3b5411a3afb8906031f543d
Jitesh Shewale <jitesh@Jiteshs-MacBook-Pro.local>
2024-09-22 22:55:34 +0530

    test.py changed

--- app1/tests.py
+++ app1/tests.py
@@ -6,10 +6,10 @@ class ModelTesting(TestCase):
         self.blog = Post.objects.create(title='Test Post', author='John Doe', slug='test-post')
 
     def test_post_creation(self):
-        post = Post.objects.get(id=1)
+        post = Post.objects.get(title='Test Post')
         self.assertEqual(post.title, 'Test Post')
         self.assertEqual(post.author, 'John Doe')
     
     def test_post_created(self):
-        post = Post.objects.get(id=1)
+        post = Post.objects.get(title='Test Post')
         self.assertIsNotNone(post.slug)
