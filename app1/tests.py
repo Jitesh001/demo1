55201f41368f91427ead8079f6f9561bce5d41d6
Jitesh Shewale <jitesh@Jiteshs-MacBook-Pro.local>
2024-09-22 22:34:14 +0530

    new model

--- app1/tests.py
+++ app1/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+from .models import Post
 # Create your tests here.
+class ModelTesting(TestCase):
+    def setUp(self):
+        self.blog = Post.objects.create(title='Test Post', author='John Doe', slug='test-post')
+
+    def test_post_creation(self):
+        post = Post.objects.get(id=1)
+        self.assertEqual(post.title, 'Test Post')
+        self.assertEqual(post.author, 'John Doe')
+    
+    def test_post_created(self):
+        post = Post.objects.get(id=1)
+        self.assertIsNotNone(post.slug)
